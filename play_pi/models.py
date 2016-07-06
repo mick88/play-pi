41c4f18587b317c9a7cbe60f4924417aeee6baa7
michaldabski <contact@michaldabski.com>
2016-07-06 22:50:46 +0100

    Add __unicode__ to models

--- play_pi/models.py
+++ play_pi/models.py
@@ -5,12 +5,18 @@ class Artist(models.Model):
 	name = models.CharField(max_length=200, unique=True)
 	art_url = models.CharField(max_length=200)
 
+	def __unicode__(self):
+		return self.name
+
 
 class Album(models.Model):
 	name = models.CharField(max_length=200)
 	artist = models.ForeignKey(Artist)
 	year = models.IntegerField(default=0)
 	art_url = models.CharField(max_length=200)
+
+	def __unicode__(self):
+		return self.name
 	
 	class Meta:
 		unique_together = ("name", "artist")
@@ -24,10 +30,16 @@ class Track(models.Model):
 	track_no = models.IntegerField(default=0)
 	mpd_id = models.IntegerField(default=0)
 
+	def __unicode__(self):
+		return self.name
+
 
 class Playlist(models.Model):
 	name = models.CharField(max_length=200)
 	pid = models.CharField(max_length=200)
+
+	def __unicode__(self):
+		return self.name
 
 	def get_art(self):
 		pc = PlaylistConnection.objects.filter(playlist=self)[0]
@@ -41,3 +53,9 @@ class Playlist(models.Model):
 class PlaylistConnection(models.Model):
 	track = models.ForeignKey(Track)
 	playlist = models.ForeignKey(Playlist)
+
+	def __unicode__(self):
+		return u'{playlist} / {track}'.format(
+			track=self.track,
+			playlist=self.playlist,
+		)
