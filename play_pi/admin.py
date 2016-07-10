02577df1a5b29c23451987aa26c80e8faf4c1606
michaldabski <contact@michaldabski.com>
2016-07-10 18:10:33 +0100

    Add admin

--- play_pi/admin.py
+++ play_pi/admin.py
@@ -5,3 +5,8 @@ from models import *
 @admin.register(Artist, Album, Track, Playlist, PlaylistConnection)
 class defaultAdmin(admin.ModelAdmin):
     pass
+
+
+@admin.register(GoogleCredentials)
+class CredentialsAdmin(admin.ModelAdmin):
+    list_display = ['username', 'enable']
