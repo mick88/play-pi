93ba6cd5726765369aa1e141445e81332945cf86
michaldabski <contact@michaldabski.com>
2016-07-18 23:38:05 +0100

    Remove the additional references to nonexisting static and template dirs

--- play_pi/settings.py
+++ play_pi/settings.py
@@ -76,14 +76,6 @@ STATIC_ROOT = ''
 # Example: "http://example.com/static/", "http://static.example.com/"
 STATIC_URL = '/static/'
 
-# Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    PROJECT_PATH + '/static',
-)
-
 # List of finder classes that know how to find static files in
 # various locations.
 STATICFILES_FINDERS = (
@@ -121,13 +113,6 @@ ROOT_URLCONF = 'play_pi.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'play_pi.wsgi.application'
-
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    PROJECT_PATH + '/templates',
-)
 
 INSTALLED_APPS = (
     'django.contrib.auth',
