33b11fe82cab849688d37a059a65e8e9108200ed
michaldabski <contact@michaldabski.com>
2016-07-20 00:06:51 +0100

    Use absolute patch to database

--- play_pi/settings.py
+++ play_pi/settings.py
@@ -26,7 +26,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'play-pi.db',                      # Or path to database file if using sqlite3.
+        'NAME': os.path.join(PROJECT_PATH, 'play-pi.db'),                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
