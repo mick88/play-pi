199845ca78a64cced84510a008a0b556a29caf2c
michaldabski <contact@michaldabski.com>
2016-10-24 23:16:14 +0100

    Reorganize urls so that API auth urls are visible to DRF

--- api/urls.py
+++ api/urls.py
@@ -10,11 +10,15 @@ api_router.register(r'radio_stations', RadioViewSet)
 api_router.register(r'albums', AlbumViewSet)
 api_router.register(r'playlists', PlaylistViewSet)
 
-urlpatterns = [
-    url(r'^auth/', include('rest_framework.urls', namespace='rest_framework')),
+api_urls = [
     url(r'^status/$', MpdStatusViewSet.as_view(), name='mpd_status'),
     url(r'^queue/(?P<position>\d+)/$', QueueView.as_view(), name='queue'),
     url(r'^queue/$', QueueView.as_view(), name='queue'),
     url(r'^play/(?P<content_type>tracks|radios)$', PlayView.as_view(), name='play'),
     url(r'^', include(api_router.urls)),
+]
+
+urlpatterns = [
+    url(r'^auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^', include(api_urls, namespace='api')),
 ]
