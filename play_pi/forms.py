258576e037002a3572dff5b9fd3aa5be7fdcf393
michaldabski <contact@michaldabski.com>
2016-11-06 16:17:54 +0000

    Implement track search

--- play_pi/forms.py
+++ play_pi/forms.py
@@ -1,9 +1,10 @@
 from __future__ import unicode_literals
 
 from django import forms
+from django.db.models.query_utils import Q
 from django.forms.widgets import PasswordInput
 
-from play_pi.models import GoogleCredentials
+from play_pi.models import GoogleCredentials, Track
 
 
 class GoogleCredentialsForm(forms.ModelForm):
@@ -14,3 +15,23 @@ class GoogleCredentialsForm(forms.ModelForm):
         widgets = {
             'password': PasswordInput(),
         }
+
+
+class SearchForm(forms.Form):
+    q = forms.CharField(max_length=70, label='Search...')
+
+    def build_track_q(self):
+        """
+        Builds Q object for filtering tracks
+        """
+        query = self.cleaned_data['q']
+        q = Q()
+        q |= Q(name__icontains=query)
+        q |= Q(artist__name__icontains=query)
+        return q
+
+    def filter(self, qs):
+        """
+        Filters queryset by filters specified in this form
+        """
+        return qs.filter(self.build_track_q())
